8b54f6999ea1ac17494085ace770f48a7cb7ffaa
Òscar Rovira López <krosk@dhcp50-231-166.externes.intranet.urv.es>
2016-02-14 11:57:07 +0100

    Seguim...

--- includes/DbHandler.php
+++ includes/DbHandler.php
@@ -6,12 +6,17 @@
 class DbHandler {
 
     private $conn;
+    private $logged_in = false;
 
     function __construct() {
         require_once dirname(__FILE__) . '/DbConnect.php';
         // abriendo conexión
         $db = new DbConnect();
         $this->conn = $db->connect();
+        session_start();
+        if (isset($_SESSION['user'])) {
+          $logged_in = true;
+        }
     }
 
     /* métodos para la tabla usuarios */
@@ -52,6 +57,55 @@ class DbHandler {
             return OBJECT_ALREADY_EXISTS;
         }
     }
+
+    /**
+    * Iniciar sesión
+    * @param String email
+    * @param String password
+    */
+    public function iniciarSesion($email, $password) {
+      $stmt = $this->conn->prepare("SELECT id FROM Usuarios WHERE email=? AND password=? LIMIT 1");
+      $stmt->bind_param("ss", $email, $password);
+      $stmt->execute();
+      $stmt->store_result();
+      $num_rows = $stmt->num_rows;
+      if($num_rows > 0) {
+        $_SESSION['user'] = $stmt->fetch_row()[0];
+        $logged_in = true;
+        return true;
+      } else {
+        $logged_in = false;
+        if(isset($_SESSION['user']) unset($_SESSION['user']);
+        return false;
+      }
+      $stmt->close();
+    }
+
+    /**
+    * Comprobar login
+    * @return Boolean Iniciada sesión
+    */
+    public function comprobarLogin() {
+      return $logged_in;
+    }
+
+    /**
+    * Obtener id usuario iniciado
+    * @return Integer id usuario
+    */
+    public function obteneridSesionUsuario() {
+      if(isset($_SESSION['user') && $logged_in) return $_SESSION['user'];
+      return NULL;
+    }
+
+    /**
+    * Cerrar sesión
+    */
+    public function cerrarSesion(){
+      session_destroy();
+      $logged_in = false;
+    }
+
 
     /**
      * Buscar usuarios
@@ -196,8 +250,48 @@ class DbHandler {
      return $num_rows > 0;
  }
 
+ /**
+  * Obtener copisteria por id
+  * @param Integer $id id de la copisteria
+  */
+ public function obtenerCopisteria($id) {
+     $stmt = $this->conn->prepare("SELECT id, email, nombre, latitud, longitud, direccion FROM Copisterias WHERE id = ?");
+     $stmt->bind_param("i", $id);
+     if ($stmt->execute()) {
+         $result = $stmt->get_result()->fetch_array(MYSQLI_ASSOC);
+         $stmt->close();
+         return $result;
+     } else {
+         return NULL;
+     }
+ }
 
+ /* métodos para la tabla Tarifas */
+ /**
+ * Obtener Tarifas de una copisteria
+ * @param Integer id de la Copisteria
+ * @param String orden ("precio" o "tiempo")
+ */
+ public function obtenerTarifasCopisteria($id, $orden) {
+     return $this->conn->query('SELECT id, email, dni FROM Copisterias WHERE id=$id ORDER BY $orden ASC');
+ }
 
+ /* métodos para la tabla Valoracion_copisterias */
+ /**
+ * Obtener valoracion media de una copisteria
+ * @param Integer id de la Copisteria
+ */
+ public function obtenerValoracionMediaCopisteria($id) {
+   return $this->conn->query('SELECT AVG(valoracion) AS media FROM Valoracion_copisterias WHERE idCopisteria=$id GROUP BY idCopisteria')->fetch_row()[0];
+ }
+
+ /* métodos para la tabla Valoracion_usuarios */
+ /**
+ * Obtener valoracion media de un usuario
+ * @param Integer id del usuario
+ */
+ public function obtenerValoracionMediaUsuario($id) {
+   return $this->conn->query('SELECT AVG(valoracion) AS media FROM Valoracion_usuarios WHERE idUsuario=$id GROUP BY idUsuario')->fetch_row()[0];
+ }
 }
-
 ?>
